61f0214de080f9d046816922771a244536dcb3d5
Dipesh Shrestha <51238569+deep-essence347@users.noreply.github.com>
2022-08-19 21:04:19 +0545

    doumented

--- Missionaries_and_Cannibals/mac.py
+++ Missionaries_and_Cannibals/mac.py
@@ -14,6 +14,12 @@ class MissionariesAndCannibal:
 		self.figure = graphviz.Digraph('state_space',filename='state_space.gv')
 
 	def generateNewNodes(self,node):
+		"""
+		It takes a node, finds all the nodes that can be reached from it, and adds them to the graph
+		
+		:param node: The current node
+		:return: The return value is a list of tuples.
+		"""
 		nodes = []
 		if(self.visitedNodes.count(node)):
 			return []
@@ -32,8 +38,13 @@ class MissionariesAndCannibal:
 			self.visitedNodes.append(node)
 			return nodes
 
-
 	def findPath(self,node):
+		"""
+		It returns a list of all possible states that can be reached from the current state
+		
+		:param node: The current node
+		:return: A list of possible moves
+		"""
 		possible=[]
 		for shift in self.shifts:
 			pm, pc = shift
@@ -56,10 +67,27 @@ class MissionariesAndCannibal:
 		return possible
 		
 	def checkGoal(self,node):
+		"""
+		If the number of missionaries and cannibals on the left side of the river is zero, then the goal
+		has been reached
+		
+		:param node: a tuple of (m,c,p) where m is the number of missionaries on the left bank, c is the
+		number of cannibals on the left bank, and p is the boat's location (0 for left, 1 for right)
+		:return: a boolean value.
+		"""
 		m,c,_ = node
 		return (not m) and (not c)
 		
 	def addNode(self, currentNode, childNode, shift, isGoal):
+		"""
+		It adds a node to the graph, and if the node is a goal node, it makes it an ellipse, otherwise it
+		makes it a rectangle.
+		
+		:param currentNode: the current node
+		:param childNode: the node to be added
+		:param shift: count for the missionaries and cannibals that are being shifted to the left
+		:param isGoal: True if the node is a goal node, False otherwise
+		"""
 		pc, pm, pp = currentNode
 		cc, cm, cp = childNode
 		sc, sm = shift
@@ -70,6 +98,13 @@ class MissionariesAndCannibal:
 		self.figure.edge(self.nodeFormat.format(pc,pm,pp), self.nodeFormat.format(cc,cm,cp), label="({}, {})".format(sc,sm))
 
 	def solve(self):
+		"""
+		It takes the first node in the graph, generates all the possible nodes that can be reached from
+		that node, and then checks if any of those nodes are the goal node. If none of them are the goal
+		node, it takes the next node in the graph and repeats the process.
+		Finally, It generates and opens an image named 'Mac.png' where the generated graph is available.
+		:return: The graph is being returned.
+		"""
 		currentRow = 0
 		goalReached = False
 		while(len(self.graph[currentRow]) and not goalReached):
@@ -91,7 +126,6 @@ class MissionariesAndCannibal:
 			self.graph.append(nodes)
 		self.figure.render(filename='MaC', format="png", view=True)
 		return self.graph
-
 
 if __name__ == '__main__':
 	mac = MissionariesAndCannibal()
